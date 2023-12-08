9bd5a674223fc149e94d9b5d27a5690baccde46f
Ian <iansutton3000@gmail.com>
2023-12-07 22:48:20 -0600

    Add js for rock paper scissors game
    Add the code for one game of rock paper scissors on page refresh

---
+++ rps.js
@@ -0,0 +1,56 @@
+// make a randomizer for the computers choice
+// get the users input and convert it to the proper case
+// compare the users input to the computers
+// alert if the user has won lost, or tied
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    //generates a random number between 0 and 3
+    if (num == 0) {
+        return "Rock";
+    }
+    else if (num == 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+
+const userInput = prompt("Type Rock Paper or Scissors:");
+//get users input
+const playerSelection = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+//converts users input to proper case to ignore case errors
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    //determine the computers input
+    if (playerSelection == computerSelection) {
+        alert(playerSelection + " ties with " + computerSelection);
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        alert("You win! " + playerSelection + " beats " + computerSelection);
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        alert("You Lose! " + computerSelection + " beats " + playerSelection);
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        alert("You win! " + playerSelection + " beats " + computerSelection);
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        alert("You Lose! " + computerSelection + " beats " + playerSelection);
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        alert("You win! " + playerSelection + " beats " + computerSelection);
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        alert("You Lose! " + computerSelection + " beats " + playerSelection);
+    }
+    else {
+        alert("An Error has occured")
+    }
+}
+
+playRound(playerSelection);
+// shows result after input is made
