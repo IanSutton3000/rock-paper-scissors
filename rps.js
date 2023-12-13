83a81f24664d84776782b0f9edda75c746aa6e1e
Ian <iansutton3000@gmail.com>
2023-12-12 20:06:05 -0600

    Add score system

--- rps.js
+++ rps.js
@@ -5,7 +5,7 @@
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
-    //generates a random number between 0 and 3
+    //generates a random number between 0 and 2
     if (num == 0) {
         return "Rock";
     }
@@ -25,30 +25,39 @@ function getUserInput() {
     return playerSelection;
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
     //determine the computers input
     if (playerSelection == computerSelection) {
         alert(playerSelection + " ties with " + computerSelection);
+        return playerScore++;
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors") {
         alert("You win! " + playerSelection + " beats " + computerSelection);
+        return playerScore++;
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
         alert("You Lose! " + computerSelection + " beats " + playerSelection);
+        return computerScore++;
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock") {
         alert("You win! " + playerSelection + " beats " + computerSelection);
+        return playerScore++;
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors") {
         alert("You Lose! " + computerSelection + " beats " + playerSelection);
+        return computerScore++;
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper") {
         alert("You win! " + playerSelection + " beats " + computerSelection);
+        return playerScore++;
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock") {
         alert("You Lose! " + computerSelection + " beats " + playerSelection);
+        return computerScore++;
     }
     else {
         alert("An Error has occured")
@@ -56,14 +65,45 @@ function playRound(playerSelection) {
 }
 
 function game() {
-    playerSelection = getUserInput();
-    playRound(playerSelection);
-    playerSelection = getUserInput();
-    playRound(playerSelection);
-    playerSelection = getUserInput();
-    playRound(playerSelection);
-    playerSelection = getUserInput();
-    playRound(playerSelection);
-    playerSelection = getUserInput();
-    playRound(playerSelection);
+    if (playerScore < 5 && computerScore < 5) {
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+        playerSelection = getUserInput();
+        playRound(playerSelection);
+        alert("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    }
+    else if (playerScore >= 5) {
+        alert("You Win The Game!");
+    }
+    else if (computerScore >= 5) {
+        alert("You Lose The Game!");
+    }
+    else {
+        alert("error");
+    }
 }
